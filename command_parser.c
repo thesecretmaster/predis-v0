04df5538596851d2e8dd51e520642577765a3d3d
thesecretmaster <thesecretmaster@developingtechnician.com>
2019-09-14 14:48:44 -0500

    Further optimized command_parser and fixed bug

--- command_parser.c
+++ command_parser.c
@@ -37,7 +37,31 @@ char *strtok_ptr(char *str, char *delims, char **ptr) {
   return rval;
 }
 
+static bool hshs_are_set = false;
+
+static unsigned int iget_hsh = 0;
+static unsigned int iset_hsh = 0;
+static unsigned int set_hsh = 0;
+static unsigned int get_hsh = 0;
+static unsigned int update_hsh = 0;
+static unsigned int delete_hsh = 0;
+static unsigned int clean_hsh = 0;
+
+static void calculate_command_hashes() {
+  if (!hshs_are_set) {
+    iset_hsh = hash("iset", sizeof("iset") - 1);
+    iget_hsh = hash("iget", sizeof("iget") - 1);
+    set_hsh = hash("set", sizeof("set") - 1);
+    get_hsh = hash("get", sizeof("get") - 1);
+    update_hsh = hash("update", sizeof("update") - 1);
+    delete_hsh = hash("delete", sizeof("delete") - 1);
+    clean_hsh = hash("clean", sizeof("clean") - 1);
+    hshs_are_set = true;
+  }
+}
+
 char *parse_command(struct main_struct *ms, struct return_val *rval, char **args, int arglen) {
+  calculate_command_hashes();
   char *cmd;
   int idx;
   int errors;
@@ -47,11 +71,11 @@ char *parse_command(struct main_struct *ms, struct return_val *rval, char **args
   int cmd_len = strlen(cmd);
   if (in_word_set(cmd, cmd_len)) {
     unsigned int cmd_hsh = hash(cmd, cmd_len);
-    if (cmd_hsh == hash("iset", sizeof("iset"))) {
+    if (cmd_hsh == iset_hsh) {
       // store <type> <value>
       idx = set(args[1], ms, args[2]);
       ret_buf = print_result(idx, NULL, false);
-    } else if (cmd_hsh == hash("iget", sizeof("iget"))) {
+    } else if (cmd_hsh == iget_hsh) {
       // get <type> <index>
       idx = strtol(args[2], NULL, 10);
       errors = get(args[1], ms, rval, idx);
@@ -59,7 +83,7 @@ char *parse_command(struct main_struct *ms, struct return_val *rval, char **args
       if (errors == 1) {
         free(rval->value);
       }
-    } else if (cmd_hsh == hash("set", sizeof("set"))) {
+    } else if (cmd_hsh == set_hsh) {
       idx = set("string", ms, args[2]);
       errors = ht_store(ms->hashtable, args[1], idx);
       if (errors != 0) {
@@ -70,7 +94,7 @@ char *parse_command(struct main_struct *ms, struct return_val *rval, char **args
         ret_buf = strdup("OK");
         // ret_buf = print_result(idx, NULL, false);
       }
-    } else if (cmd_hsh == hash("get", sizeof("get"))) {
+    } else if (cmd_hsh == get_hsh) {
       idx = ht_find(ms->hashtable, args[1]);
       errors = get("string", ms, rval, idx);
       if (errors >= 0) {
@@ -82,22 +106,23 @@ char *parse_command(struct main_struct *ms, struct return_val *rval, char **args
       if (errors == 1) {
         free(rval->value);
       }
-    } else if (cmd_hsh == hash("update", sizeof("update"))) {
+    } else if (cmd_hsh == update_hsh) {
       // update <type> <updater> <index> <new value>
       idx = strtol(args[3], NULL, 10);
       errors = update(args[1], args[2], ms, args[4], idx);
       ret_buf = print_result(errors, NULL, false);
-    } else if (cmd_hsh == hash("delete", sizeof("delete"))) {
+    } else if (cmd_hsh == delete_hsh) {
       // delete <index>
       idx = strtol(args[1], NULL, 10);
       errors = del(ms, idx);
       ret_buf = print_result(errors, NULL, false);
-    } else if (cmd_hsh == hash("clean", sizeof("clean"))) {
+    } else if (cmd_hsh == clean_hsh) {
       errors = clean_queue(ms);
       output_len = snprintf(NULL, 0, "%d", errors);
       ret_buf = malloc(sizeof(command_done)+sizeof(char)*output_len+1);
       snprintf(ret_buf, sizeof(command_done)+sizeof(char)*output_len+1, command_done, errors);
     } else {
+      printf("gperf: Some kind of error occured\n");
       ret_buf = malloc(sizeof(unrecognized_command)+sizeof(char)*cmd_len+1);
       snprintf(ret_buf, sizeof(unrecognized_command)+sizeof(char)*cmd_len + 1, unrecognized_command, cmd);
     }
