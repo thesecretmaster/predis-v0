826e90c0c900360a70aed66c6246562dfaa4105b
thesecretmaster <thesecretmaster@developingtechnician.com>
2019-10-22 20:36:13 -0500

    Prep for type hashtable

---
+++ type_list_gen.rb
@@ -0,0 +1,20 @@
+ht = {}
+
+Dir["types/*"].each do |file|
+  fn = File.basename(file, ".*")
+  ht[fn] ||= 0
+  ht[fn] += 1
+end
+
+types = ht.select do |k,v|
+  v == 2 && k.ascii_only?
+end.map do |k, _v|
+  k
+end
+
+File.write("tmp/type_list.gperf", types.join("\n"))
+File.open("tmp/type_list.gperf", "a") do |f|
+  f << "\n%%\n"
+  f << File.read("ht_func.c")
+  f << "\nstatic char* in_word_set(register const char *str, register size_t len);\n"
+end
